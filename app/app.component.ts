1cf52d88240177f69da08db63492ae3aeae5486b
Riccardo Ressi <Riccardo.Ressi@gmail.com>
2016-10-22 07:41:22 +0100

    Prof of concept of the interface implemented

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,37 @@
 import { Component } from '@angular/core';
 
+let message = ""
+
+let CONTENT = `
+<h1>smartsearch-demo</h1>
+
+<div>
+This is a demo app for <a href="https://github.com/rressi/smartsearch">smartsearch</a>.
+Sources for this app can be found <a href="https://github.com/rressi/smartsearch-demo">here</a>.
+<br><br>
+<b>Note:</b> this app is still in the very first stages of development.
+</div>
+
+<br><br>
+<hr>
+<br><br>
+
+<div ng-app="my-app" ng-controller="searchForm">
+  <form novalidate>
+    <b>Query:</b>
+    <input type="text" ng-model="searchQuery">
+    <button ng-click="">OK</button>
+  </form>
+</div>
+<div>
+
+<br><br>
+Not implemented yet, sorry!
+</div>
+`
+
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular App</h1>'
+    template: CONTENT
 })
 export class AppComponent { }
